de7d1863f20543288b7110d2ddac6980a167b1db
MaximeBnl <bonnalmaxime@gmail.com>
2020-01-29 00:19:59 +0100

    Main2activity ajoutée.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
@@ -20,6 +25,10 @@ public class MainActivity extends AppCompatActivity
         Button btnQuitter = (Button) findViewById(R.id.btnQuitter);
         btnQuitter.setOnClickListener(btnQuitterOnClickListener);
         popUp("onCreate()");
+        Button btnEnvoyer = (Button) findViewById(R.id.btnEnvoyer);
+        btnEnvoyer.setOnClickListener(btnEnvoyerOnClickListener);
+        Button btnAct2 = (Button) findViewById(R.id.btnAct2);
+        btnAct2.setOnClickListener(btnQuitterOnClickListener);
     }
     /** =============================================================
      * Exécuté que l'activité arrêtée via un "stop" redémarre.
@@ -40,6 +49,8 @@ public class MainActivity extends AppCompatActivity
     protected void onStart() {
         super.onStart();
         popUp("onStart()");
+        SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);
+        setTxTValeur(settings.getString("valeur", ""));
     }
     /** ==============================================================
      * Exécutée à chaque passage en premier plan de l'activité.
@@ -52,6 +63,8 @@ public class MainActivity extends AppCompatActivity
     protected void onResume() {
         super.onResume();
         popUp("onResume()");
+        SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);
+        setTxTValeur(settings.getString("valeur", ""));
     }
     /** =============================================================
      * La fonction onPause() est suivie :
@@ -70,6 +83,10 @@ public class MainActivity extends AppCompatActivity
         } else {
             popUp("onPause, l'utilisateur n'a pas demandé la fermeture via un finish()");
         }
+        SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("valeur", getTxtValeur());
+        editor.commit();
     }
     /** ==============================================================
      * La fonction onStop() est exécutée :
@@ -107,4 +124,24 @@ public class MainActivity extends AppCompatActivity
     public void popUp(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
+
+    View.OnClickListener btnEnvoyerOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            popUp("valeur saisie = " + getTxtValeur());
+            Intent intent = new Intent(v.getContext(), Main2Activity.class);
+            startActivity(intent);
+        }
+    };
+    public String getTxtValeur() {
+        EditText zoneValeur = (EditText) findViewById(R.id.editTxtValeur);
+        return zoneValeur.getText().toString();
+    }
+
+    public void setTxTValeur(String valeur) {
+        EditText zoneValeur = (EditText) findViewById(R.id.editTxtValeur);
+        zoneValeur.setText(valeur);
+    }
+
+
 }
